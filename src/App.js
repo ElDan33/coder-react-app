3b1856f72893924c22834c360f8a390d8756b1c3
Dante Carrizo <danteenriquecarrizo@gmail.com>
2022-03-21 17:40:03 -0300

    First eCommerce app deployment

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Under construct. Dante Carrizo's eCommerce app soon...</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coderhouse.com/online/reactjs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          👨‍💻🚀 Learning React from CODERHOUSE 🚀👨‍💻
         </a>
       </header>
     </div>
